a331367b08a5803a0325033ad473921b76d3c953
Akash <akashraj6202@gmail.com>
2026-06-26 15:59:34 +0530

    Initial terraform commit

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web" {
+    ami = "ami-0b6d9d3d33ba97d99"
+    instance_type = "t2.micro"
+    subnet-id = aws_subnet.public_subnet.id
+    key_name = var.key_name
+    vpc_security_group_ids = [aws_security_group.web-sg.id]
+    tags = {
+        Name = "Terraform-server"
+    }
+}
